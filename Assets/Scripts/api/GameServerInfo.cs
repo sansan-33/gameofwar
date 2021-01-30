12b69f0148c336540b5bd2bed380131367cb52c4
imacpro <jack.33.wong@gmail.com>
2021-01-30 20:55:40 +0000

    api

---
+++ Assets/Scripts/api/GameServerInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GameServer
+{
+    public int id;
+    public string main;
+}
+[System.Serializable]
+public class GameServerInfo
+{
+    public int id;
+    public string name;
+    public List<GameServer> GameServer;
+}
