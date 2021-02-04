65f47055403aaab364f30c44e12ef592de8780c6
imacpro <jack.33.wong@gmail.com>
2021-02-04 10:44:30 +0000

    attack interface

---
+++ Assets/Scripts/Interfaces/IAttack.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+
+    /// <summary>
+    /// Interface for an agent that is able to attack.
+    /// </summary>
+    public interface IAttack 
+    {
+        /// <summary>
+        /// Returns the furthest distance that the agent is able to attack from.
+        /// </summary>
+        /// <returns>The distance that the agent can attack from.</returns>
+        void ScaleAttackDelay(int factor);
+
+        /// <summary>
+        /// Returns the maximum angle that the agent can attack from.
+        /// </summary>
+        /// <returns>The maximum angle that the agent can attack from.</returns>
+        void ScaleDamageDeal(float factor);
+
+    }
