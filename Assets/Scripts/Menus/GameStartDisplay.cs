73747ccda8e8b0cbdf3890c0cdd07be2b07d5659
Leonard Wong <leonard.11.wong@gmail.com>
2021-05-02 12:13:12 +0100

    timer

--- Assets/Scripts/Menus/GameStartDisplay.cs
+++ Assets/Scripts/Menus/GameStartDisplay.cs
@@ -4,12 +4,14 @@ using Mirror;
 using TMPro;
 using UnityEngine;
 
-public class GameStartDisplay : MonoBehaviour
+public class GameStartDisplay : NetworkBehaviour
 {
     [SerializeField] private GameObject gameStartDisplayParent = null;
     [SerializeField] private TMP_Text StartTime = null;
     [SerializeField] private TMP_Text Times = null;
+    [SyncVar(hook = "StartTimeing")]
     private float startTime = 3;
+    [SyncVar(hook = "Timeing")]
     private float Timer = 180;
     private void Start()
     {
@@ -19,35 +21,42 @@ public class GameStartDisplay : MonoBehaviour
     private void Update()
     {
         GameStartCountDown();
+        GameEndCountDown();
     }
 
-
+    [Server]
     private void GameStartCountDown()
     {
         startTime -= 1 * Time.deltaTime;
-        
+    }
+    [Server]
+    private void GameEndCountDown()
+    {
         Timer -= Time.deltaTime;
-        
-        if (startTime <= 0)
+    }
+    public void StartTimeing(float oldTime, float newTime)
+    {
+       // Debug.Log($"oldTime:{oldTime}newTime:{newTime}");
+        if (newTime <= 30 && newTime > 0)
         {
-
-            float minutes = Mathf.FloorToInt(Timer / 60);
-            float seconds = Mathf.FloorToInt(Timer % 60);
-            if (Timer <= 0) { return; }
-            Times.text = string.Format("{0:00}:{1:00}", minutes, seconds);
-            startTime = 0;
-           
+            StartTime.text = newTime.ToString("0");
         }
-        if (startTime <= 30 && startTime > 0)
-        {
-            StartTime.text = startTime.ToString("0");
-            
-            
-        }
-        else if (startTime <= 0)
+        else if (newTime <= 0)
         {
             gameStartDisplayParent.SetActive(false);
         }
     }
-   
+    public void Timeing(float oldTime, float newTime)
+    {
+        //Debug.Log($"oldTime:{oldTime}newTime:{newTime}");
+            float minutes = Mathf.FloorToInt(newTime / 60);
+            float seconds = Mathf.FloorToInt(newTime % 60);
+            if (newTime <= 0) { return; }
+            Times.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+           
+
+        
+        
+    }
+
 }
