f086fc0e67687361f45aba1039c0be615aec511b
imacpro <jack.33.wong@gmail.com>
2021-05-20 15:45:06 +0100

    Bellum Folder

---
+++ Assets/Bellum/Editor/PlayerTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+public class PlayerTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [Test]
+    public void DragCardTestSuiteSimplePasses()
+    {
+        // Use the Assert class to test conditions
+        Debug.Log($"Test Now 123");
+        GameObject obj = new GameObject();
+        obj.AddComponent<Player>();
+        obj.GetComponent<Player>().dragCardMerge();
+    }
+}
