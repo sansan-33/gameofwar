6f9e53e529d1e7173e76d5140f6ccc0cbaffc3d1
Leonard Wong <leonard.11.wong@gmail.com>
2021-06-08 20:31:36 +0100

    game [play meta

---
+++ Assets/Bellum/Scripts/I18N/GamePlayMeta.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePlayMeta : MonoBehaviour
+{
+    public static Dictionary<string, string> UnitStarSkill = new Dictionary<string,  string>()
+    {
+        {"1" , "Not saving any 3 star cards" },
+        {"2", "Not saving any 3 star cards"  },
+        {"3" , "Not saving any 3 star cards"  }
+    };
+}
