ab04f79bdf180fbe4138a8422317986e18f4720a
Leonard Wong <leonard.11.wong@gmail.com>
2021-06-27 15:18:53 +0100

    map 03 drag card layer

--- Assets/Bellum/Scripts/GameBoard/PlayerGround.cs
+++ Assets/Bellum/Scripts/GameBoard/PlayerGround.cs
@@ -7,26 +7,79 @@ public class PlayerGround : MonoBehaviour
 {
     [SerializeField] GameObject playerMesh;
     [SerializeField] GameObject enemyMesh;
+    [SerializeField] List<GameObject> layers;
+    [SerializeField] GameObject enenmyBack;
+    [SerializeField] GameObject enenmyLeft;
+    [SerializeField] GameObject enenmyCentre;
+    [SerializeField] GameObject enenmyRight;
+    [SerializeField] GameObject playerBack;
+    [SerializeField] GameObject playerLeft;
+    [SerializeField] GameObject playerCentre;
+    [SerializeField] GameObject playerRight;
+    [SerializeField] Material Grey;
     [SerializeField] private GameObject enemyHalf;
     [SerializeField] private GameObject playerHalf;
+    private bool breakLeftWall = false;
+    private bool breakCentreWall = false;
+    private bool breakRightWall = false;
     private Transform[] childTransform;
-    //private RTSPlayer player;
+    private RTSPlayer player;
     // Start is called before the first frame update
     public void Start()
     {
+        player = NetworkClient.connection.identity.GetComponent<RTSPlayer>();
+        CardDealer.FinishDealEnemyCard += SubscribeEvent;
+    }
+    private void SubscribeEvent()
+    {
+        GreatWallController.GateOpened += GateOpend;
+    }
+    private void GateOpend(string playerId, string doorIndex)
+    {
+        if(playerId == player.GetPlayerID().ToString())
+        {
+            switch (doorIndex)
+            {
+                case "0":
+                    breakLeftWall = true;
+                    Debug.Log("Break Left");
+                    break;
+                case "1":
+                    breakCentreWall = true;
+                    Debug.Log("Break Centre");
+                    break;
+                case "2":
+                    breakRightWall = true;
+                    Debug.Log("Break Right");
+                    break;
+            }
+        }
     }
     public void sortLayer(int playerID)
     {
         if (((RTSNetworkManager)NetworkManager.singleton).Players.Count == 1)
         {
 
-            enemyHalf.layer = LayerMask.NameToLayer("Floor");
+            /*enemyHalf.layer = LayerMask.NameToLayer("Floor");
             childTransform = enemyHalf.GetComponentsInChildren<Transform>();
             foreach (Transform child in childTransform)
             {
                 
                 child.gameObject.layer = LayerMask.NameToLayer("Floor");
-            }
+            }*/
+            enenmyBack.layer = LayerMask.NameToLayer("Floor");
+            enenmyCentre.layer = LayerMask.NameToLayer("Floor");
+            enenmyLeft.layer = LayerMask.NameToLayer("Floor");
+            enenmyRight.layer = LayerMask.NameToLayer("Floor");
+            //enenmyBack.SetActive(true);
+            //enenmyCentre.SetActive(true);
+            // enenmyLeft.SetActive(true);
+            // enenmyRight.SetActive(true);
+            // Debug.Log($"breakLeftWall{breakLeftWall}breakCentreWall{breakCentreWall}breakRightWall{breakRightWall}");
+            if (breakLeftWall == true) { playerLeft.layer = LayerMask.NameToLayer("Floor"); }//playerLeft.SetActive(true); }
+            if (breakCentreWall == true) { playerCentre.layer = LayerMask.NameToLayer("Floor"); } // playerCentre.SetActive(true); }
+            if (breakRightWall == true) { playerRight.layer = LayerMask.NameToLayer("Floor"); }// playerRight.SetActive(true); }
+
             playerMesh.SetActive(true);
         }
         else // Multi player seneriao
@@ -61,11 +114,15 @@ public class PlayerGround : MonoBehaviour
    
     public void resetLayer()
     {
-        foreach (Transform child in childTransform)
+        /* foreach (Transform child in childTransform)
+         {
+             child.gameObject.layer = LayerMask.NameToLayer("Default");
+         }*/
+        foreach (GameObject child in layers)
         {
             child.gameObject.layer = LayerMask.NameToLayer("Default");
         }
-        playerMesh.SetActive(false);
+      playerMesh.SetActive(false);
         enemyMesh.SetActive(false);
     }
      
