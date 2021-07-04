dc63a73148c2bb304acef8705d806d8686421f79
imacpro <jack.33.wong@gmail.com>
2021-07-04 16:06:31 +0100

    auto spin

---
+++ Assets/Bellum/Scripts/Utility/AutoRotateSprite.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using UnityEngine;
+
+public class AutoRotateSprite : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(rotateObject(objectToRotate, new Vector3(0, 0, 90), 3000f));
+    }
+
+    bool rotating = false;
+    public GameObject objectToRotate;
+
+    IEnumerator rotateObject(GameObject gameObjectToMove, Vector3 eulerAngles, float duration)
+    {
+        if (rotating)
+        {
+            yield break;
+        }
+        rotating = true;
+
+        Vector3 newRot = gameObjectToMove.transform.eulerAngles + eulerAngles;
+
+        Vector3 currentRot = gameObjectToMove.transform.eulerAngles;
+
+        float counter = 0;
+        while (counter < duration)
+        {
+            counter += Time.deltaTime;
+            gameObjectToMove.transform.eulerAngles = Vector3.Lerp(currentRot, newRot, counter / duration);
+            yield return null;
+        }
+        rotating = false;
+    }
+}
