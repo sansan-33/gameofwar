974bc03491f27a6739ec1b7d8affe899e580024c
imacpro <jack.33.wong@gmail.com>
2021-07-19 14:00:16 +0100

    beacon

---
+++ Assets/Bellum/Scripts/Buildings/Beacon.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using UnityEngine;
+using Mirror;
+
+public class Beacon : MonoBehaviour
+{
+    private UnitFactory localFactory;
+
+    private int playerID = 0;
+    private Color teamColor;
+    private CardDealer cardDealer;
+    public Transform barrackTransform;
+
+    void Start()
+    {
+        if (NetworkClient.connection.identity == null) { return; }
+        RTSPlayer player = NetworkClient.connection.identity.GetComponent<RTSPlayer>();
+        playerID = player.GetPlayerID();
+        teamColor = player.GetTeamColor();
+        PowerUpTeam();
+    }
+    public void OnDestroy()
+    {
+    }
+    public void PowerUpTeam()
+    {
+        foreach (GameObject factroy in GameObject.FindGameObjectsWithTag("UnitFactory"))
+        {
+            if (factroy.GetComponent<UnitFactory>().hasAuthority)
+            {
+                localFactory = factroy.GetComponent<UnitFactory>();
+                StartCoroutine(HandlePowerUpTeam(UnitMeta.UnitType.FOOTMAN, playerID, teamColor, StaticClass.playerRace));
+            }
+        }
+    }
+    IEnumerator HandlePowerUpTeam(UnitMeta.UnitType unitType, int _playerid, Color _teamColor, UnitMeta.Race race)
+    {
+        cardDealer = GameObject.FindGameObjectWithTag("DealManager").GetComponent<CardDealer>();
+        CardStats cardStats;
+        while (true)
+        {
+            cardStats = cardDealer.userCardStatsDict[UnitMeta.UnitRaceTypeKey[race][unitType].ToString()];
+            localFactory.CmdDropUnit(_playerid, barrackTransform.position, race, unitType, unitType.ToString(), 1, cardStats.cardLevel, cardStats.health, cardStats.attack, cardStats.repeatAttackDelay, cardStats.speed, cardStats.defense, cardStats.special, cardStats.specialkey, cardStats.passivekey, 1, _teamColor, Quaternion.identity);
+            yield return new WaitForSeconds(1.5f);
+        }
+    }
+
+}
