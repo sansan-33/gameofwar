d1145b4bd118e310475d51d5f34577007eb10d8f
imacpro <jack.33.wong@gmail.com>
2021-07-25 19:50:19 +0100

    nerf cavalry

--- Assets/Bellum/Scripts/Units/UnitWeapon.cs
+++ Assets/Bellum/Scripts/Units/UnitWeapon.cs
@@ -20,7 +20,7 @@ public class UnitWeapon : NetworkBehaviour, IAttackAgent, IAttack
     private Vector3 weaponSize = new Vector3(0.15f, 0.1f, 0.5f);
     private float calculatedDamageToDeal ;
     private float originalDamage;
-    public float DashDamageFactor = 0.05f;
+    public float DashDamageFactor = 0.1f;
     public bool IsKingSP = false;
     NetworkIdentity opponentIdentity;
     bool m_Started;
@@ -104,7 +104,7 @@ public class UnitWeapon : NetworkBehaviour, IAttackAgent, IAttack
                 if (unit.unitType == UnitMeta.UnitType.CAVALRY && unit.GetUnitMovement().GetAcceleration() > 0f)
                 {
                     Debug.Log($"Unit Weapon speed current: {unit.GetUnitMovement().GetSpeed(UnitMeta.SpeedType.CURRENT)}  max: {unit.GetUnitMovement().GetSpeed(UnitMeta.SpeedType.MAX)} calculatedDamageToDeal {calculatedDamageToDeal} * {DashDamageFactor}");
-                    calculatedDamageToDeal *= (DashDamageFactor + unit.GetUnitMovement().GetSpeed(UnitMeta.SpeedType.CURRENT));
+                    calculatedDamageToDeal *= (DashDamageFactor * unit.GetUnitMovement().GetSpeed(UnitMeta.SpeedType.CURRENT));
                     cmdDamageText(other.transform.position, calculatedDamageToDeal, originalDamage, opponentIdentity, isFlipped);
                 }
                 //yield return new WaitForSeconds(GetComponent<IAttack>().RepeatAttackDelay() - .6f);
