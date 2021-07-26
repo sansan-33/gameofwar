6d4df5ed053113761bd7a1401f390a5fe7afdf53
Leonard Wong <leonard.11.wong@gmail.com>
2021-07-26 15:55:17 +0100

    AI close fog and sp fog

--- Assets/Bellum/Scripts/SpecialAttack/SpButtonManager.cs
+++ Assets/Bellum/Scripts/SpecialAttack/SpButtonManager.cs
@@ -340,7 +340,7 @@ public class SpButtonManager : MonoBehaviour
             spType = SpecialAttackType.BOMB;
         }*/
 
-        //spType = SpecialAttackType.STUN;
+        spType = SpecialAttackType.DARKNESSRETURN;
 
 
         var impectSmash = Instantiate(impectSmashPrefab, button.transform);
